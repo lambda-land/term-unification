9d6f6273b6cfaa74bcb2882e39c705ea35cd41aa
pithlessly <79002325+pithlessly@users.noreply.github.com>
2024-05-08 13:32:33 -0700

    add Rule/Rules data types for a rule system

--- src/Rules.hs
+++ src/Rules.hs
@@ -1,1 +1,13 @@
-module Rules where
+module Rules
+    ( Rule (..), Rules
+    ) where
+
+import Common
+
+data Rule = Rule
+  { conclusion :: !Judgement
+  , premises   :: ![Judgement]
+  }
+
+type Rules = [( String -- name
+              , Rule )]
