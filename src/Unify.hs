89e4293c2a6afccaa20532fd9eeccf7f702fb50e
pithlessly <79002325+pithlessly@users.noreply.github.com>
2024-05-08 15:28:40 -0700

    add fairer conjunction operator with example usage

--- src/Unify.hs
+++ src/Unify.hs
@@ -1,5 +1,7 @@
 module Unify
     ( unify
+    , search
+    , tes
     ) where
 
 import qualified Control.Monad.State.Strict as State
@@ -11,6 +13,7 @@ import Control.Monad (forM_, guard)
 
 import Logic.Proof (Proof (..), pattern Proof)
 
+import Control.Monad.Backtrack (allResults)
 import Common
 import Rules (Rule (..), Rules, instantiate)
 
@@ -39,6 +42,25 @@ occurs v t = do
   case t' of
     Var v'    -> pure (v == v')
     Term _ ts -> anyM (occurs v) ts
+
+-- fairer conjunction (>>- is still not fair enough)
+(>>≠) :: MonadLogic m => m a -> (a -> m b) -> m b
+a >>≠ k = step [] [] (Just a) where
+  -- step :: MonadLogic m => [m b] -> [m b] -> Maybe (m a) -> m b
+  step strip' (nextB : strip) nextA = do
+    mb <- msplit nextB
+    case mb of
+      Nothing          ->            step           strip'  strip nextA
+      Just (b, nextB') -> pure b <|> step (nextB' : strip') strip nextA
+  step []     [] Nothing =
+    empty
+  step strip' [] Nothing =
+    step [] (reverse strip') Nothing
+  step strip' [] (Just nextA) = do
+    mb <- msplit nextA
+    case mb of
+      Nothing          -> step []       (reverse strip') Nothing
+      Just (a, nextA') -> step [] (k a : reverse strip') (Just nextA')
 
 unify :: Term -> Term -> Unif ()
 unify t1 t2 = do
@@ -72,3 +94,10 @@ search rules j =
                                >>- \premiseTrees ->
   inspectDeep j                >>= \j' ->
   pure (Proof j' premiseTrees)
+
+tes :: IO ()
+tes = print . map fst . fst $
+  allResults (choose [5, 6, 7, 8] >>≠ \n ->
+                         choose (take n ['A'..]) >>- \c ->
+                         pure (n, c))
+             () ()
